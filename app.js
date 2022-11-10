c360d0367ee47e0d2486cc67f864f7911d051d02
ANKIY4 <chimariyaankit234@gmail.com>
2022-11-10 17:56:48 +0545

    added enviroment variables

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -15,8 +16,10 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = "this is our little secret.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"],
+});
 
 const user = mongoose.model("user", userSchema);
 
